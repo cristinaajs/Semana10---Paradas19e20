bd2bddc6088797a7e4d5169621f2a5348ac8b76d
Cristina Jesus <cristinaaajs@gmail.com>
2024-05-11 09:37:28 -0300

    Semana10-Desafio10.1

---
+++ parada19/introducao2.js
@@ -0,0 +1,10 @@
+/*var curso = " "
+var duracao = " "
+
+curso = prompt('Qual curso você faz ou quer fazer na faculdade')
+duração = promp('Qual a duração do curso')
+
+console.log('Você quer fazer $(curso) com duração de $(duracao) anos') */
+
+var texto = "Olá mundo!"
+document.getElementById
---
+++ parada20/codigo.js
@@ -0,0 +1,9 @@
+let semaforo = "vermelho"
+
+if (semaforo == "verde") {
+    alert("Siga em frente!")
+} else if (semaforo== "amarelo") {
+    alert("Atenção!")
+} else {
+    console.log ("Espere!")
+} 
